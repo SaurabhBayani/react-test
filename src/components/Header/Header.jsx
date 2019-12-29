c5cbc7a2a6460edf2341435898be7c4b5810c070
Saurabh Bayani <saurabh.bayani@gmail.com>
2019-12-29 20:06:35 +0530

    Optimization for tests

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,10 @@ import Logo from './../../assets/graphics/logo.png';
 
 const Header = (props) => {
     return (
-        <header className="headerComponent">
+        <header className="headerComponent" data-test="headerComponent">
             <div className="wrap">
                 <div className="logo">
-                    <img className="logoIMG" src={Logo} alt="Logo" />
+                    <img className="logoIMG" data-test="logoIMG" src={Logo} alt="Logo" />
                 </div>
             </div>
         </header>
--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -2,18 +2,32 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Header from './Header';
 
+// to optimize our test file we can collect all components in single place
+const setUp = (props = {}) => {
+    const component = shallow(<Header />);
+    return component;
+}
+
+const findByTestAttr = (component, attr) => {
+    const wrapper = component.find(`[data-test="${attr}"]`);
+    return wrapper;
+};
+
 // We can describe set of tests using decribe method
-describe('Header component', ()=>{
+describe('Header component', () => {
+    let component;
+    // Before each runs before every test
+    beforeEach(() => {
+        component = setUp();
+    });
     // We can use both it or test method, they both are almost same
     it('Should render header without error', () => {
-        const headerComponent = shallow(<Header />);
-        const headerClass = headerComponent.find('.headerComponent')
+        const headerClass = findByTestAttr(component, 'headerComponent');
         expect(headerClass.length).toBe(1);
     });
 
-    it('Should render logo without error', ()=>{
-        const headerComponent = shallow(<Header />);
-        const logoClass = headerComponent.find('.logoIMG')
+    it('Should render logo without error', () => {
+        const logoClass = findByTestAttr(component, 'logoIMG');
         expect(logoClass.length).toBe(1);
     });
 });
